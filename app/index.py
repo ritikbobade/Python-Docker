227ab72f094545a90eb7636f9b0b5f92b9561b9f
“ritikbobade” <ritikbobade@gmail.com>
2021-05-20 21:39:48 +0530

    third committ

--- app/index.py
+++ app/index.py
@@ -4,15 +4,8 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def factorial(n):
-     
-    # single line to find factorial
-    return 1 if (n==1 or n==0) else n * factorial(n - 1);
- 
-# Driver Code
-num = 5;
-print("Factorial of",num,"is",
-factorial(num))
+def hello_world():
+    return "This is test page"
 
 
 app.run(host='0.0.0.0', port=5000)
